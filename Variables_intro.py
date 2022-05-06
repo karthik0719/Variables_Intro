9a14cc8c7c6dc0862547199f2ff8c8121287c926
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-06 17:36:32 +0530

    Add files via upload

---
+++ Variables_intro.py
@@ -0,0 +1,39 @@
+"""
+Name: Karthik
+Date: 6th May 2022
+Day: Friday
+Time: 4:30-5:30PM
+File Description: About Variables
+"""
+''' The following are the variables can be used in starting, Middle and end'''
+# a=1
+# A=2
+# _b=3
+# _1=4
+# c_a=5
+# v_3=6
+# _=7
+# x_y3=8
+# print(A)
+# print(_b)
+# print(_1) 
+# print(c_a)
+# print(v_3)
+# print(_)
+# print(x_y3)
+# print(ABC) # NameError: name 'ABC' is not defined
+
+''' The following are the rules which are not valid '''
+"""
+.a=5
+@=7
+7_@4=6
+$q=8
+2_=9
+r t=10
+f 
+"""
+
+''' To check whether variabe is true or false '''
+print('hi'.isidentifier())
+
